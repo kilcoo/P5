99a77e0a0f27b389e330dd23b0aed959f3178365
kilco <jacques61593@gmail.com>
2022-04-03 15:35:27 +0200

    suppression produit ajuster

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,33 +1,35 @@
 let productId = new URL(window.location.href).searchParams.get("id");
-panier = JSON.parse(localStorage.getItem("produit"));                                                         // on recupere les donner dans le localstorage
+let panier = JSON.parse(localStorage.getItem("produit"));                                                         // on recupere les donner dans le localstorage
 let products;                                                                                                 // on declare une variable product
+let product;
 let panierAfficher = [];
 const getProduct = async () => {                                                                              // on cree une constante pour obtenir les produits sur le site
     await fetch("http://localhost:3000/api/products/")                                                        // ajout des produits sur le site avec l'api en utilisant fetch
-    .then((res) => res.json())                                                                                // reponse
-    .then((JSON) => (products = JSON))                                                                        // on demande que les produits sont afficher en format json
-    .catch((err) => console.log(err));                                                                        // pour afficher les erreurs si il y a des erreurs
-  console.log(products);                                                                                      // on demande a afficher les produits dans la console
+        .then((res) => res.json())                                                                                // reponse
+        .then((JSON) => (products = JSON))                                                                        // on demande que les produits sont afficher en format json
+        .catch((err) => console.log(err));                                                                        // pour afficher les erreurs si il y a des erreurs
+    console.log(products);                                                                                      // on demande a afficher les produits dans la console
 
-  const elementPanier = document.querySelector("#cart__items");                      // on cree une variable element panier puis on selectionne la div contenant la class cart items
-  if (panier === null) {                                                             // si le panier est vide
-    let panierVide;                                                                  // on declare une nouvelle variable pour le panier vide
-    elementPanier.innerHTML = panierVide;                                            // on definit element panier = parent de panier vide
-  } else {                                                                           // sinon
-    panierAfficher = [];                                                             // on cree un nouveau tableau pour afficher le panier sur la page
-    for (i = 0; i < panier.length; i++) {                                            // on cree une boucle qui sera repeter pour chaque nouveau element
-      panierAfficher =                                                            // on dit que panier afficher = le tableau + le html avec des element pris dans l'api + localstorage
-        panierAfficher +
-        `
+    const elementPanier = document.querySelector("#cart__items");                      // on cree une variable element panier puis on selectionne la div contenant la class cart items
+    if (panier === null) {                                                             // si le panier est vide
+        let panierVide;                                                                  // on declare une nouvelle variable pour le panier vide
+        elementPanier.innerHTML = panierVide;                                            // on definit element panier = parent de panier vide
+    } else {                                                                           // sinon
+        panierAfficher = [];                                                             // on cree un nouveau tableau pour afficher le panier sur la page
+        for (i = 0; i < panier.length; i++) {                                            // on cree une boucle qui sera repeter pour chaque nouveau element
+            product = products.filter(prod => prod._id === panier[i].id);
+            panierAfficher =                                                            // on dit que panier afficher = le tableau + le html avec des element pris dans l'api + localstorage
+                panierAfficher +
+                `
       <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
       <div class="cart__item__img">
-        <img src="${products[i].imageUrl}" alt="Photographie d'un canapé">
+        <img src="${product[0].imageUrl}" alt="Photographie d'un canapé">
       </div>
       <div class="cart__item__content">
         <div class="cart__item__content__description">
-          <h2>${products[i].name}</h2>
+          <h2>${product[0].name}</h2>
           <p>${panier[i].color}</p>
-          <p>${products[i].price} €</p>
+          <p>${product[0].price} €</p>
         </div>
         <div class="cart__item__content__settings">
           <div class="cart__item__content__settings__quantity">
@@ -41,25 +43,25 @@ const getProduct = async () => {                                                
       </div>
     </article>
     `;
-    }
-    if (i === panier.length) {                                                        // on ajoute les elements
-      elementPanier.innerHTML = panierAfficher;
-    }
+        }
+        if (i === panier.length) {                                                        // on ajoute les elements
+            elementPanier.innerHTML = panierAfficher;
+        }
 
-    // supprimer element
+        // supprimer element
 
-    let supprimer = document.querySelectorAll(".deleteItem");                         // on declare une variable pour supprimer en selectionner la class du bouton supprimer
-    console.log(supprimer);                                                           // on verifie si la variable et bien lier au bouton supprimer
-    for (let q = 0; q < supprimer.length; q++){                                       // on cree une boucle 
-       supprimer[q].addEventListener("click", (event) =>{                             // on va mettre un add event listener pour le bouton supprimer
-        event.preventDefault();                                                       // pour ne pas recharger la page
-        let elementSupprimer = panier[q].id;                                          // on cree une variable qui sera supprimer en utilisant l'id
-        panier = panier.filter(element => element.id !== elementSupprimer)            // on utilise la methode filter pour retirer l'element ou j'ai appuyer sur supprimer
-        localStorage.setItem("produit" ,JSON.stringify(panier));                      // on met a jour la clee
-        window.location.href = "cart.html";                                           // on actualise la page
-      });
+        let supprimer = document.querySelectorAll(".deleteItem");                         // on declare une variable pour supprimer en selectionner la class du bouton supprimer
+        console.log(supprimer);                                                           // on verifie si la variable et bien lier au bouton supprimer
+        for (let q = 0; q < supprimer.length; q++){                                       // on cree une boucle
+            supprimer[q].addEventListener("click", (event) =>{                             // on va mettre un add event listener pour le bouton supprimer
+                event.preventDefault();                                                       // pour ne pas recharger la page
+                let elementSupprimer = panier[q].id;                                          // on cree une variable qui sera supprimer en utilisant l'id
+                panier = panier.filter(element => element.id !== elementSupprimer)            // on utilise la methode filter pour retirer l'element ou j'ai appuyer sur supprimer
+                localStorage.setItem("produit" ,JSON.stringify(panier));                      // on met a jour la clee
+                window.location.href = "cart.html";                                           // on actualise la page
+            });
+        }
     }
-  }
 };
 
 getProduct();
